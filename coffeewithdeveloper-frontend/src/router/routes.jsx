dedebb3fecb0b21edbe3050da038ff0dfa16a427
Tayeb SOUINI <TayebsouiniYcode@gmail.com>
2024-04-28 13:31:06 +0200

    clean react architecture

--- coffeewithdeveloper-frontend/src/router/routes.jsx
+++ coffeewithdeveloper-frontend/src/router/routes.jsx
@@ -3,6 +3,7 @@ import {createBrowserRouter} from "react-router-dom";
 import FullLayout from "../components/layouts/FullLayout";
 import Login from "../pages/login";
 import Home from "../pages/Home";
+import DashboardLayout from "../components/layouts/DashboardLayout";
 
 export const router = createBrowserRouter( [
     {
@@ -23,6 +24,15 @@ export const router = createBrowserRouter( [
             {
                 path: "/",
                 element: <Home />
+            }
+        ]
+    },
+    {
+        element: <DashboardLayout />,
+        children : [
+            {
+                path: "/dashboard",
+                element: <div>Test</div>
             }
         ]
     }
