fd57d50d4cbb18bd1d40a5992b733476cae2ffc4
Tayeb SOUINI <TayebsouiniYcode@gmail.com>
2024-05-13 11:20:10 +0200

    ajouter Card component

--- coffeewithdeveloper-frontend/src/pages/Home.jsx
+++ coffeewithdeveloper-frontend/src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import FullLayout from "../components/layouts/FullLayout";
 import HeroSection from "../components/herosection/HeroSection";
 
 export default function Home() {
