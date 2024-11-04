2b81c310023f9e25d77946e49cc1e5c95680d13b
Tayeb SOUINI <TayebsouiniYcode@gmail.com>
2024-11-04 11:10:15 +0100

    Revert "ajouter Card component"
    This reverts commit fd57d50d4cbb18bd1d40a5992b733476cae2ffc4.

--- coffeewithdeveloper-frontend/src/pages/Home.jsx
+++ coffeewithdeveloper-frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import FullLayout from "../components/layouts/FullLayout";
 import HeroSection from "../components/herosection/HeroSection";
 
 export default function Home() {
